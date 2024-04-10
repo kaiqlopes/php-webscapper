7f411880114dc12405383ca9a0c5b61313dd5b8c
Kaique Lopes <kai-quelopes@hotmail.com>
2024-04-10 14:58:09 -0300

    All necessary data collected for each html page

--- src/WebScrapping/Scrapper.php
+++ src/WebScrapping/Scrapper.php
@@ -15,52 +15,79 @@ class Scrapper {
    */
   public function scrap(\DOMDocument $dom): array {
       $xPath = new \DOMXPath($dom);
-      $query = "//a[contains(@class, 'paper-card')]";
-      $links = $xPath->query($query);
+      $links = $xPath->query("//a[contains(@class, 'paper-card')]");
       $ids = $xPath->query("//div[contains(@class, 'volume-info')]");
       $linkIterator = 0;
 
-      $href = $links->item(0)->getAttribute('href');
-      $linkHtml = file_get_contents($href);
+      foreach ($links as $link) {
 
-      $linkDom = new \DOMDocument();
-      $linkDom->loadHTML($linkHtml);
-      $linkXPath = new \DOMXPath($linkDom);
+          $href = $link->getAttribute('href');
+          $linkHtml = file_get_contents($href);
 
-      $id = $ids->item($linkIterator)->nodeValue;
-      $title = $linkDom->getElementsByTagName('h2')->item(1)->nodeValue;
-      $presentationType = $linkDom->getElementsByTagName('strong')->item(2)->nodeValue;
+          $linkDom = new \DOMDocument();
+          $linkDom->loadHTML($linkHtml);
+          $linkXPath = new \DOMXPath($linkDom);
 
-      $duplicatedAuthors = $linkXPath->query("//div[contains(@class, 'authors-wrapper')]//abbr");
+          $id = $ids->item($linkIterator)->nodeValue;
+          $title = $xPath->query("//h4[contains(@class, 'paper-title')]")->item($linkIterator)->nodeValue;
+          $presentationSearch = $xPath->query("//div[contains(@class, 'tags')][1]");
+          $presentationType = $presentationSearch->item($linkIterator)->nodeValue;
+          $linkIterator++;
 
-      $authorsQuantity = $duplicatedAuthors->length / 2;
-      $authors = [];
+          $authors = [];
+          $universities = [];
 
-      for($i = 0; $i < $authorsQuantity; $i++) {
-          $authors[] = $duplicatedAuthors->item($i)->nodeValue;
-      }
+          if ($presentationType == "Poster Presentation") {
+              $authorsSearch = $linkXPath->query("//div[contains(@class, 'authors-wrapper')]//li");
+              $authorsQuantity = $authorsSearch->length / 2;
 
-      echo "\nId: " . $id . "\n";
-      echo "Title: " . $title . "\n";
-      echo "Presentation Type: " . $presentationType . "\n";
-      echo "Authors: ";
-      foreach ($authors as $author) {
-          echo $author . ", ";
+              for ($i = 0; $i < $authorsQuantity; $i++) {
+                  $authors[] = $authorsSearch->item($i)->nodeValue;
+              }
+
+              $universitiesSearch = $linkXPath->query("//div[contains(@class, 'f-gray')]//li");
+              $universitiesQuantity = $universitiesSearch->length / 2;
+
+              for ($i = 0; $i < $universitiesQuantity; $i++) {
+                  $universities[] = $universitiesSearch->item($i)->nodeValue;
+              }
+          }
+          else {
+              $authorsSearch = $linkXPath->query("//div[contains(@class, 'row')]//ul[contains(@class, 'list')]//li");
+              $authorsQuantity = $authorsSearch->length / 2;
+
+              for ($i = 0; $i < $authorsQuantity; $i++) {
+                  $authors[] = $authorsSearch->item($i)->nodeValue;
+              }
+
+              $universitiesSearch = $linkXPath->query("//div[contains(@class, 'panel-body')]//div[contains(@class, 'form-group')]//li");
+              for ($i = 0; $i < $universitiesSearch->length; $i++) {
+                  $universities[] = $universitiesSearch->item($i)->nodeValue;
+              }
+          }
+
+
+
+          echo "\nId: " . $id . "\n";
+          echo "Title: " . $title . "\n";
+          echo "Presentation Type: " . $presentationType . "\n";
+          echo "Authors: ";
+
+          foreach ($authors as $author) {
+              echo $author . ", ";
+          }
+          echo "\n";
+
+          echo "Universities: ";
+          foreach ($universities as $university) {
+              echo $university . ", ";
+          }
+          echo "\n";
+
       }
 
       echo "\n";
-      /*if ($links->length > 0) {
-
-          foreach ($links as $link) {
-
-          }
-
-      }
-      else {
-          echo "Nothing was found";
-      }*/
 
       return [];
   }
-
 }
