04ba422683f1582bd936639225d397b8fc44283e
Kaique Lopes <kai-quelopes@hotmail.com>
2024-04-15 13:46:34 -0300

    Adding User-Agent

--- src/WebScrapping/Scrapper.php
+++ src/WebScrapping/Scrapper.php
@@ -20,12 +20,18 @@ class Scrapper {
       $ids = $xPath->query("//div[contains(@class, 'volume-info')]");
       $linkIterator = 0;
 
+      $context = stream_context_create([
+          'http' => [
+              'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+          ]
+      ]);
+
       if($links->length > 0) {
 
           foreach ($links as $link) {
 
               $href = $link->getAttribute('href');
-              $linkHtml = file_get_contents($href);
+              $linkHtml = file_get_contents($href, true, $context);
 
               $linkDom = new \DOMDocument();
               $linkDom->loadHTML($linkHtml);
